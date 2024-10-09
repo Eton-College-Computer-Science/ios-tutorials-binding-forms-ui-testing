d18873bd72a5e53d2e4f98c4d8f66201ab9dfa05
David Cormell <d.cormell@etoncollege.org.uk>
2024-10-09 16:12:28 +0100

    Starter code for User input and Binding

--- RateMyTeacher/ContentView.swift
+++ RateMyTeacher/ContentView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
+enum TeacherRating: String {
+    case poor, average, excellent
+}
+
 struct ContentView: View {
+    @State private var teacherRating: TeacherRating?
+    @State private var areasForImprovement: String = ""
+    @State private var enthusiasm: Int = 5
+    @State private var subjectExpertise: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Rate!") {
+                rateMyTeacher()
+            }
+            Text("Teacher Rating: \(teacherRating?.rawValue ?? "Not yet calculated")")
         }
         .padding()
+    }
+    
+    func rateMyTeacher() {
+        if subjectExpertise == "Knows their stuff" && enthusiasm > 7 && areasForImprovement.count <= 10 {
+            teacherRating = .excellent
+        } else if subjectExpertise == "Not Bad" && enthusiasm > 4 {
+            teacherRating = .average
+        } else {
+            teacherRating = .poor
+        }
     }
 }
 
