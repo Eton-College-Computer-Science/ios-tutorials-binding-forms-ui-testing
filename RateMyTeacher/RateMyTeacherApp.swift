493df68b60f543cf3119e79db92d9df1c1f5a272
David Cormell <d.cormell@etoncollege.org.uk>
2024-10-09 16:03:15 +0100

    Initial Commit

---
+++ RateMyTeacher/RateMyTeacherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RateMyTeacherApp.swift
+//  RateMyTeacher
+//
+//  Created by Cormell, David - DPC on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RateMyTeacherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
